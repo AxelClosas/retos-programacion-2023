ca01f0dc93c56f6b3dbfdec645dcd36622f4fcce
axel <axelclosas13@gmail.com>
2023-10-03 20:16:05 -0300

    Primera versión utilizando listas y funciones

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/axelclosas.py
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/axelclosas.py
@@ -1,10 +1,4 @@
 '''
-# Reto #1: EL "LENGUAJE HACKER"
-#### Dificultad: Fácil | Publicación: 02/01/23 | Corrección: 09/01/23
-
-## Enunciado
-
-```
 /*
  * Escribe un programa que reciba un texto y transforme lenguaje natural a
  * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
@@ -13,36 +7,8 @@
  *   con el alfabeto y los números en "leet".
  *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
  */
-```
-#### Tienes toda la información extendida sobre los retos de programación semanales en **[retosdeprogramacion.com/semanales2023](https://retosdeprogramacion.com/semanales2023)**.
-
-Sigue las **[instrucciones](../../README.md)**, consulta las correcciones y aporta la tuya propia utilizando el lenguaje de programación que quieras.
-
-> Recuerda que cada semana se publica un nuevo ejercicio y se corrige el de la semana anterior en directo desde **[Twitch](https://twitch.tv/mouredev)**. Tienes el horario en la sección "eventos" del servidor de **[Discord](https://discord.gg/mouredev)**.
-
-    Objetivos:
-    1. Lograr la transformación con letras unicamente
-    2. Incluir transformación de números
-
-    
-    Traducción basica:
-    Reemplaza las vocales nada más 
-
-    Traducción Intermedia
-    Reemplaza vocales y consonantes
-
-    Traducción Avanzada:
-    Reemplaza vocales, consonantes y números
 '''
 
-
-vocales = {
-    'A': 4,
-    'E': 3,
-    'I': 1,
-    'O': 0,
-    'U': '(_)',
-}
 alfabeto = {
     'A': 4,
     'B': 'I3',
@@ -82,14 +48,6 @@ alfabeto = {
     0: 'o',
 }
 
-# def checkType():
-#     pass
-
-
-# def invertLowerToUpper(c):
-#     if c.isUpper():
-#         pass
-
 # Retorna una lista de caracteres en base a la lista de palabras recibidas.
 def returnToCharacterList(words_list):
     charList = []
@@ -107,7 +65,11 @@ def convertTextToLeetSpeak(cadena):
         if char == ' ':
             result.append(char)
             continue
+
         if char in alfabeto.keys():
+            result.append(alfabeto[char])
+        elif char.isnumeric():
+            char = int(char)
             result.append(alfabeto[char])
 
     return result
@@ -115,6 +77,7 @@ def convertTextToLeetSpeak(cadena):
 
 def main():
     # Mensaje de bienvenida
+    print("\n")
     print("Bienvenidx. \n\n Este es un software que te permite traducir un mensaje escrito en lenguaje natural a lenguaje leet (1337).\n\n")
 
     # Solicitamos al usuario que ingrese el texto que desea transformar y las convertimos en mayusculas
@@ -136,19 +99,12 @@ def main():
     # Transformamos el mensaje mediante la función convertTextToLeetSpeak que recibe como argumento el texto ingresado. Retorna una lista.
     msg_leet_on_list = convertTextToLeetSpeak(words_list)
 
-    print(msg_leet_on_list)
-
-    
-    # Recibo el parametro c y retorno de manera implicita la conversión a string 
-    # convert = lambda c : str(c)
-
-    # Llamo la función convert y reemplazo los valores de salida mediante un nuevo list comprehension
-    # 2da versión. Directamente convierto el valor mediante la función str()
-
-    mensaje_leet = "".join([str(c) for c in msg_leet_on_list])
+    # Aplicando el metodo .join(<iterable>), que recibe un iterable como argumento, le podemos pasar un list comprehension para transformar un list a un string
+    mensaje_leet = "".join([str(c) for c in msg_leet_on_list]).strip()
 
     print("Mensaje 1337 (or leet):\n", mensaje_leet, "\n")
 
 
 if __name__ == '__main__':
+
     main()
